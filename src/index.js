d00408e634ae7fee9c554090d03cef769ce694d8
rossy <63504188+rossy-IT0164@users.noreply.github.com>
2021-05-04 22:47:23 +0900

    Create

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+//Reactのコンポーネントを使用する場合は必須（JSXのみの場合は必須ではない）
+import React from "react";
+//htmlDOMを使用するためのライブラリ
+import ReactDom from "react-dom";
+import App from "./App.js";
 
+// JSX記法；JavaScript内にhtmlタグを埋め込む記法
+/** コンポーネント化してApp.jsへ移動
 const App = () => {
- retrun (
-  <React.Fragment>
-    <h1>こんにちは</h1>
-    <p>おげんきですか？</p>
+ return (
+//  ホントはこれ<React.Fragment>。下記は短縮形
+
+ <>
+  <h1>こんにちは</h1>
+  <p>おげんきですか？</p>
+ </>
+
+// </React.Fragment>
  );
 };
-
+*/
+//render(<反映したいコンポーネント>,<反映する先のDOM要素>)
 ReactDom.render(<App />, document.getElementById("root"));
-
